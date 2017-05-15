7df65094e4c7c4b9728169c305678de5032c2cf6
Mustafa Akin <mustafa91@gmail.com>
2017-05-15 18:55:31 +0300

    some test code

--- deneme/wow.go
+++ deneme/wow.go
@@ -1,8 +1,6 @@
 package deneme
 
 import (
-	"fmt"
-
 	"github.com/mustafaakin/gongular2"
 )
 
@@ -18,6 +16,5 @@ type SelamMid struct {
 }
 
 func (a *SelamMid) Handle(c *gongular2.Context) error {
-	fmt.Println("im mid")
 	return nil
 }
